6fbae3e1b0fff3b572c30c793e7a4e2feb7edd5a
kndunlap <kylendunlap19@gmail.com>
2024-03-25 12:44:12 -0600

    First pandas commit

---
+++ pandas.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 12:37:10 2024
+
+@author: u1314571
+"""
+
+import os
+os.chdir('/Users/u1314571/Desktop')
+os.chdir('Python_Organized')
+os.chdir('pandas-learn')
